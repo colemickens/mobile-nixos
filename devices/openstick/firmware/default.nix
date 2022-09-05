806989dc19845ccf8ace82d1f3dfb38e544eb1ae
Piotr Dobrowolski <admin@tastycode.pl>
2022-09-06 01:48:25 +0200

    openstick: firmware: extract binaries directly from vendor blobs

--- devices/openstick/firmware/default.nix
+++ devices/openstick/firmware/default.nix
@@ -1,35 +1,34 @@
 { lib
 , runCommand
 , fetchurl
-, fetchgit
-, unzip
 , e2fsprogs
-, simg2img
+, mtools
+, linux-firmware
 }:
 
 let
-  upstreamImage = fetchurl {
-    url = "https://github.com/OpenStick/OpenStick/releases/download/v1/debian.zip";
-    sha256 = "sha256-i+bxAOj8P2EuCwPBmdAfUdOLlToZNDrQ9M96L3pX9gM=";
+  modemImage = fetchurl {
+    url = "https://github.com/Informatic/stick-blobs/raw/abdf16d3aa2e415c6c41062a5e2575cdc7b70fd8/stock-uf896_v1_1/modem.bin";
+    sha256 = "sha256-LB5nwSbVTxPgFb+a+NcSgx3F6SC1FJX1IVc7bLIWNXM=";
   };
-in runCommand "openstick-firmware" {
-  nativeBuildInputs = [ e2fsprogs mtools unzip simg2img];
+  wlannvImage = fetchurl {
+    url = "https://github.com/Informatic/stick-blobs/raw/abdf16d3aa2e415c6c41062a5e2575cdc7b70fd8/stock-uf896_v1_1/persist.bin";
+    sha256 = "sha256-3xW6e0eKEpeR1Nz4uQGG0BIWzEVq/o7jtpSZvgRIiX0=";
+  };
+in runCommandNoCC "openstick-firmware" {
+  nativeBuildInputs = [ e2fsprogs mtools ];
   meta.license = [
     # We make no claims that it can be redistributed.
     lib.licenses.unfree
   ];
 } ''
-  unzip ${upstreamImage} debian/rootfs.img
-  cd debian
-  simg2img rootfs.img vendor-raw.img
-  debugfs vendor-raw.img -R "rdump lib/firmware ."
-  
-  ls -al -R .
+  mkdir -p $out/lib/firmware/wlan/prima/
+  debugfs ${wlannvImage} -R "rdump WCNSS_qcom_wlan_nv.bin $out/lib/firmware/wlan/prima/"
 
-  mkdir -p $out/lib
-  mv firmware $out/lib/firmware
-  
-  echo "--"
-  ls -al $out/lib/firmware
-  # exit -1
+  mcopy -v -s -i ${modemImage} '::image/modem.*' '::image/mba.*' '::image/wcnss.*' $out/lib/firmware/
+
+  mkdir -p $out/lib/firmware/qcom/
+  cp -r ${linux-firmware}/lib/firmware/qcom/{venus-1.8,a300_pfp.fw,a300_pm4.fw} $out/lib/firmware/qcom/
+
+  ls -al -R $out/lib/firmware
 ''
