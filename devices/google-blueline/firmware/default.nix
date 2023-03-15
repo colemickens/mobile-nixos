samueldr-wip/mobile-nixos-wip

google-blueline: init
As this is, it will not work; the SDM845 mainline main branch doesn't
have support for the pixel 3 at this time.

Stay tuned for an incoming commit!

devices/google-blueline/firmware/default.nix
@@ -1,0 +1,29 @@
+{ runCommandNoCC
+, fetchFromGitLab
+, firmwareLinuxNonfree
+, wireless-regdb
+}:
+
+runCommandNoCC "google-blueline-firmware" {
+  src = firmwareLinuxNonfree;
+  sdm845_mainline = fetchFromGitLab {
+    owner = "sdm845-mainline";
+    repo = "firmware-google-pixel3";
+    rev = "7deb5f8e0b0499a80ea85dfc7941351c77e7738c"; # main
+    hash = "sha256-eb4hI/mMA7BqcH2YVcIgZ+IpdljWo7/dnq0rqXam0jQ=";
+  };
+} ''
+  mkdir -p $out/lib/firmware
+
+  cp -vrf -t $out/lib/firmware $sdm845_mainline/lib/firmware/*
+  chmod -R +w $out/lib/firmware
+
+  (
+    cd $out/lib/firmware
+    mv -t ./     postmarketos/ath10k
+    mv -t ./qca/ postmarketos/qca/*
+  )
+
+  # Firmware we can get from upstream
+  cp -vt $out/lib/firmware ${wireless-regdb}/lib/firmware/regulatory.db*
+''

colemickens/mobile-nixos

devices/google-blueline/firmware/default.nix
@@ -1,0 +1,29 @@
+{ runCommand
+, fetchFromGitLab
+, firmwareLinuxNonfree
+, wireless-regdb
+}:
+
+runCommand "google-blueline-firmware" {
+  src = firmwareLinuxNonfree;
+  sdm845_mainline = fetchFromGitLab {
+    owner = "sdm845-mainline";
+    repo = "firmware-google-pixel3";
+    rev = "7deb5f8e0b0499a80ea85dfc7941351c77e7738c"; # main
+    hash = "sha256-eb4hI/mMA7BqcH2YVcIgZ+IpdljWo7/dnq0rqXam0jQ=";
+  };
+} ''
+  mkdir -p $out/lib/firmware
+
+  cp -vrf -t $out/lib/firmware $sdm845_mainline/lib/firmware/*
+  chmod -R +w $out/lib/firmware
+
+  (
+    cd $out/lib/firmware
+    mv -t ./     postmarketos/ath10k
+    mv -t ./qca/ postmarketos/qca/*
+  )
+
+  # Firmware we can get from upstream
+  cp -vt $out/lib/firmware ${wireless-regdb}/lib/firmware/regulatory.db*
+''